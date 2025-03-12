033f44ba09503dfa6fd1cf0001515889027f49f1
wshin025 <wody0506@gmail.com>
2025-03-12 11:27:51 +0900

    Initial commit

---
+++ src/test/java/kr/ac/kopo/wsk/bookmarket/BookMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.wsk.bookmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
