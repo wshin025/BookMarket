17c8378b7601e04142d3c57ffe5bb3db2d5bc4fc
wshin025 <wody0506@gmail.com>
2025-11-03 11:49:40 +0900

    BookMarket의 test2

--- src/main/resources/static/js/controllers.js
+++ src/main/resources/static/js/controllers.js
@@ -1,20 +1,26 @@
-function addToCart(bookId){
-    if(confirm("장바구니에 도서를 추가할까요?")){
-        document.addForm.action = "/BookMarket/cart/book/" + bookId;
+function addToCart(bookid)
+{
+
+    if (confirm("장바구니에 도서를 추가합니까?")){
+
+        document.addForm.action="/BookMarket/cart/book/"+bookid;
         document.addForm.submit();
     }
-}
 
-function removeFromCart(bookId, cartId){
-    document.removeForm.action = "/BookMarket/cart/book/" + bookId;
+
+}
+function removeFromCart(bookid, cartId) {
+
+    document.removeForm.action = "/BookMarket/cart/book/"+bookid;
     document.removeForm.submit();
-    setTimeout('location.reload()', 10);
+    setTimeout('location.reload()',10);
 }
 
+function clearCart(cartId) {
+    if (confirm("모든 도서를 장바구니에서 삭제합니까?")==true){
 
-function deleteCart(cartId){
-    if(confirm('모든 도서를 장바구니에서 삭제하시겠습니까?')){
-        document.deleteForm.submit();
-        setTimeout('location.reload()', 10);
+        document.clearForm.submit();
+        setTimeout('location.reload()',10);
+
     }
 }
